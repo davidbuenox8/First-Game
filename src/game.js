a74e2735231f224b2b354fb9c3dbe25eb5cb12dc
David Bueno <davidbuenox8@gmail.com>
2021-04-07 16:53:12 +0200

    done

--- src/game.js
+++ src/game.js
@@ -18,10 +18,10 @@ class Game {
 
     preload() {
         //images
-        this.backgroundImage = loadImage('./images/background1.png');
-        this.playerImageS = loadImage('./images/player.png')
-        this.playerImageR = loadImage('./images/playerRight.png');
-        this.playerImageL = loadImage('./images/playerLeft.png');
+        this.backgroundImage = loadImage('images/background1.png');
+        this.playerImageS = loadImage('images/player.png')
+        this.playerImageR = loadImage('images/playerRight.png');
+        this.playerImageL = loadImage('images/playerLeft.png');
         this.playerJump = loadImage('./images/playerjump.png');
         this.friendImage = loadImage('./images/rescue2.png');
         //  this.friendImage1 = loadImage('../images/rescue1.png');
